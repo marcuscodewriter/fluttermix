ecf62f7a120a6cdd99e6e2889a4ff3be0723c4dd
M3 Beatz <m3beatz@M3.local>
2024-08-06 19:02:24 -0400

    Ready for beta release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "63262127"
+    serviceWorkerVersion: "4079207041"
   }
 });
